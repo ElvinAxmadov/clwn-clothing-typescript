3c94bac8247b070aeb3d861abda05045c060f5d3
ElvinAxmadov <110697153+ElvinAxmadov@users.noreply.github.com>
2023-04-14 14:20:09 +0400

    Initial commit

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,45 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
+import {
+    CheckoutContainer,
+    CheckoutHeader,
+    HeaderBlock,
+    Total,
+} from './checkout.styles';
+import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
+
+const Checkout = () => {
+    const dispatch = useDispatch();
+    const cartItems = useSelector(selectCartItems);
+    const cartTotal = useSelector(selectCartTotal);
+
+    return (
+        <CheckoutContainer>
+            <CheckoutHeader>
+                <HeaderBlock>
+                    <span>Product</span>
+                </HeaderBlock>
+                <HeaderBlock>
+                    <span>Description</span>
+                </HeaderBlock>
+                <HeaderBlock>
+                    <span>Quantity</span>
+                </HeaderBlock>
+                <HeaderBlock>
+                    <span>Price</span>
+                </HeaderBlock>
+                <HeaderBlock>
+                    <span>Remove</span>
+                </HeaderBlock>
+            </CheckoutHeader>
+            {cartItems.map((cartItem) => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))}
+            <Total>Total: ${cartTotal}</Total>
+        </CheckoutContainer>
+    );
+};
+
+export default Checkout;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,136 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgvfUVIxm74lxvX4wmtLi19bkIBELdzzo",
+  authDomain: "crwn-clothing-db-8a689.firebaseapp.com",
+  databaseURL: "https://crwn-clothing-db-8a689-default-rtdb.firebaseio.com",
+  projectId: "crwn-clothing-db-8a689",
+  storageBucket: "crwn-clothing-db-8a689.appspot.com",
+  messagingSenderId: "400070981293",
+  appId: "1:400070981293:web:e9a9adf50a8415bc5f7824",
+};
+
+// eslint-disable-next-line no-unused-vars
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd,
+  field
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("done");
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
+};
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userSnapshot;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (userAuth) => {
+        unsubscribe();
+        resolve(userAuth);
+      },
+      reject
+    );
+  });
+};
